725ac282203c5c164c412a7a1707b912f1d38942
franxx321 <francruy@gmail.com>
2023-09-17 19:25:19 -0300

    Agregue las clases del modelo de dominio, falta definir que nombre le ponemos a la clase de asociacion que le llamamos precio, hay que definir que atributos le damos a Paciente

--- src/main/java/Objetos/Ingreso.java
+++ src/main/java/Objetos/Ingreso.java
@@ -18,4 +18,60 @@ public class Ingreso {
     private Paciente paciente;
 
     private ObraSocial obraSocial;
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Profesional getProfesional() {
+        return profesional;
+    }
+
+    public void setProfesional(Profesional profesional) {
+        this.profesional = profesional;
+    }
+
+    public List<Turno> getTurnos() {
+        return turnos;
+    }
+
+    public void setTurnos(List<Turno> turnos) {
+        this.turnos = turnos;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public ObraSocial getObraSocial() {
+        return obraSocial;
+    }
+
+    public void setObraSocial(ObraSocial obraSocial) {
+        this.obraSocial = obraSocial;
+    }
 }

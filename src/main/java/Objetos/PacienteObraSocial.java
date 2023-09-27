ef957b5c3e302f14c272c421ea8e7ecb68e1fdac
franxx321 <francruy@gmail.com>
2023-09-26 21:04:14 -0300

    retoques a DAOs y a las clases del modelo de dominio

---
+++ src/main/java/Objetos/PacienteObraSocial.java
@@ -0,0 +1,21 @@
+package Objetos;
+
+public class PacienteObraSocial {
+    private int idObraSocial, idPaciente;
+
+    public int getIdObraSocial() {
+        return idObraSocial;
+    }
+
+    public void setIdObraSocial(int idObraSocial) {
+        this.idObraSocial = idObraSocial;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+}

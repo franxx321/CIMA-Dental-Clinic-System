63e2ca02455c82c39939d29d21e3774bb651a52e
franxx321 <francruy@gmail.com>
2023-11-14 16:06:52 -0300

    retoques por varios lados

--- src/main/java/GUIComponents/MenuModificarTurno.java
+++ src/main/java/GUIComponents/MenuModificarTurno.java
@@ -354,7 +354,7 @@ public class MenuModificarTurno extends Panel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addGap(0, 0, 0)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(flechaIzqButton, javax.swing.GroupLayout.Alignment.TRAILING)
                                     .addComponent(flechaDerechaButton)))
@@ -367,7 +367,7 @@ public class MenuModificarTurno extends Panel {
                         .addComponent(cancelarButton, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 365, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 365, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
